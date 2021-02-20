82a9afd87e3c9e0f803aeffdcc877ddc263f6f4f
MrCejota <carlos.j.sola@gmail.com>
2021-02-20 13:45:29 -0500

    init2

--- LS_CAN_Read.ino
+++ LS_CAN_Read.ino
@@ -3,7 +3,7 @@
 
 struct can_frame canMsg;
 MCP2515 mcp2515(10);
-//byte can_Address;
+byte can_Address;
 
 void setup() {
   Serial.begin(115200);
@@ -12,13 +12,13 @@ void setup() {
   mcp2515.setBitrate(CAN_33KBPS);
   mcp2515.setNormalMode();
   Serial.println("Type CAN ID to look for: e.g. 0x044097");
-  //can_Address = Serial.read();
+  can_Address = Serial.read();
   Serial.println("------- CAN Read ----------");
   Serial.println("ID  DLC   DATA");
 }
 
 void loop() {
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK /*&& canMsg.can_id == can_Address*/) {
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK && canMsg.can_id == can_Address) {
     Serial.print(canMsg.can_id, HEX); // print ID
     Serial.print(" "); 
     Serial.print(canMsg.can_dlc, HEX); // print DLC
